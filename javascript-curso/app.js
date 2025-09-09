84c07836915b423667754fddaa8f5b480ef34ff2
Rodrigo-Abed-Cruz-Diaz <124049938@upq.edu.mx>
2025-09-08 21:52:27 -0600

    Tipos de datos de JS
    Se vieron los tipos de datos de JavaScript vistos en el video para principiantes

--- javascript-curso/app.js
+++ javascript-curso/app.js
@@ -1,1 +1,32 @@
-document.write("<h1>Hello world</h1>");
+//tipos de datos
+
+"Hello world" //String
+'Hello world' //String
+
+100000 //number
+-2.3 //number
+
+//boolean
+true
+false
+
+//array
+['joe', 'ryan', 'martha']
+['1', '2', '3']
+[true, false, true, false]
+
+//object 
+
+{
+    username: 'ryan'
+    score: 70.4
+    hours: 14
+    proffesional: true
+}
+
+{
+    username: 'joe'
+    score: 0.4
+    hours: 1
+    proffesional: false
+}
