0d908a3e2277e69a6ffc7eb5b517d9a9882e25aa
Rodrigo-Abed-Cruz-Diaz <124049938@upq.edu.mx>
2025-11-25 21:04:54 -0600

    Practica 18 Insert y Select con SQLite Local
    Desarrolle la practica 18 que consistia en aplicar el insert y select con un base de SQLite de forma local, tuve problemas con algunas dependencias por lo que tuve que hacer algunas cosas extras a las explicadas por mi profesor en la practica

---
+++ userDB/App.js
@@ -0,0 +1,8 @@
+import { StatusBar } from 'expo-status-bar';
+import InsertUsuarioScreen from './screens/InsertarUsuarioScreen';
+
+export default function App() {
+  
+  return <InsertUsuarioScreen/>                             
+  
+}
